988599bc499f4b959f0ddda10467f89e736f3574
loushark <lou.m.reade@gmail.com>
2021-02-05 16:48:26 +0000

    Learning ruby

---
+++ Practice.rb
@@ -0,0 +1,23 @@
+=begin
+
+#printing numbers 1 -10
+
+  number = 9
+
+  while number < 100 do
+    number = number+1
+    puts number
+  end
+
+=end
+
+
+number = 1
+
+while number < 100
+  if number.odd?
+    puts number
+  end
+
+  number = number+1
+end
