dd0ad6d0bf2862c8c94092aebcf9065606003e94
loushark <lou.m.reade@gmail.com>
2021-02-24 18:44:05 +0000

    organising files

---
+++ censoring_bad_tweets.rb
@@ -0,0 +1,21 @@
+
+#I'm a client working for the Blank House. We want to display positive tweets about our president on our website. However, our president is kind of unpopular, and we pretty much only receive negative press. Write me a program that filters out the following words from tweets: "sucks", "bad", "hate", "foolish", and the most popular: "danger to society". Replace each negative word or phrase them with the word "CENSORED". Some test tweets have been provided for you.
+
+test_tweets = [
+  "This president sucks!",
+  "I hate this Blank House!",
+  "I can't believe we're living with such a bad leadership. We were so foolish",
+  "President Presidentname is a danger to society. I hate that he's so bad – it sucks."
+  ]
+
+banned_phrases = ["sucks", "bad", "hate", "foolish", "danger to society"]
+
+test_tweets.map do |tweet|
+ words = tweet.split
+ words.map do |word|
+   banned_phrases.map do |bad|
+     word.sub!(bad, "CENSORED")
+   end
+ end
+  puts words.join(" ")
+end
